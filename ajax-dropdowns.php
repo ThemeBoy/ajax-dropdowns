aff582b4ebac9db99d8074c407a4600bb10136de
ThemeBoy <support@themeboy.com>
2014-08-28 01:12:40 +1000

    Tag version 0.9.5

--- ajax-dropdowns.php
+++ ajax-dropdowns.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Ajax_Dropdowns
- * @version 0.9.4
+ * @version 0.9.5
  */
 /*
 Plugin Name: Ajax Dropdowns
 Plugin URI: http://wordpress.org/plugins/ajax-dropdowns/
 Description: Display a group of posts that can be switched using dropdowns.
 Author: ThemeBoy
-Version: 0.9.4
+Version: 0.9.5
 Author URI: http://themeboy.com/
 */
 
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Plugin setup
  *
- * @since 0.9.4
+ * @since 0.9
 */
 class Ajax_Dropdowns {
 
@@ -51,7 +51,7 @@ class Ajax_Dropdowns {
 	*/
 	private function define_constants() {
 		if ( !defined( 'AJAX_DROPDOWNS_VERSION' ) )
-			define( 'AJAX_DROPDOWNS_VERSION', '0.9.4' );
+			define( 'AJAX_DROPDOWNS_VERSION', '0.9.5' );
 
 		if ( !defined( 'AJAX_DROPDOWNS_URL' ) )
 			define( 'AJAX_DROPDOWNS_URL', plugin_dir_url( __FILE__ ) );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ThemeBoy, brianmiyaji
 Tags: ajax, dropdowns, option, posts, pages, custom post types, tabs, groups, redirect, inline
 Requires at least: 3.0
 Tested up to: 3.9
-Stable tag: 0.9.4
+Stable tag: 0.9.5
 License: GPLv2 or later
 
 Display a group of posts that can be switched using dropdowns.
@@ -61,6 +61,9 @@ Automatic updates should work like a charm; as always though, ensure you backup 
 6. A dropdown is displayed on the frontend to swap the content below it.
 
 == Changelog ==
+
+= 0.9.5 =
+* Feature - Add no content option to display dropdown only in redirect mode.
 
 = 0.9.4 =
 * Feature - Enable bulk adding all posts of a post type.
